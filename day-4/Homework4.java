348aaf9b9ebf97116b23d5ed61680586233cde45
Ayush Kumar Nath <developer.ayushknath@gmail.com>
2023-01-21 23:34:15 +0530

    Day 5 homework by Ayush Kumar Nath

--- day-4/Homework4.java
+++ day-4/Homework4.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Homework {
+public class Homework4 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n: ");
